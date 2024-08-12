c624767623855b8eb37b2b2e8247b12f9665158c
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-08-12 10:13:03 -0600

    Changes

--- src/evalDir.py
+++ src/evalDir.py
@@ -1,167 +1,109 @@
 from pathlib import Path
-import numpy as np
 import sys
 import argparse
-import time
-from math import ceil
-from os import listdir, makedirs, remove
-from os.path import join, exists
-from PIL import Image
-import io
-from haar2D import fwdHaarDWT2D
-from train import createElements, defineEpochRange, scaleData
 import tensorflow as tf
+from os import listdir
+from os.path import join, basename
+import numpy as np
+from sklearn import preprocessing
+from PIL import Image
+import pywt
+import pywt.data
+import io
+import json
 
-def load_model(model_path):
-    model_extension = Path(model_path).suffix.lower()
-
-    if model_extension in ['.h5', '.keras']:
-        model = tf.keras.models.load_model(model_path, compile=False)
-        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-        print(f"Modelo {model_extension} encontrado y cargado correctamente!\n")
-        return model
-    
-    elif model_extension == '.tflite':
-        interpreter = tf.lite.Interpreter(model_path=model_path)
-        interpreter.allocate_tensors()
-        print("Modelo .tflite encontrado y cargado correctamente!\n")
-        return interpreter
-        
-    else:
-        raise ValueError(f"Unsupported model file format: {model_extension}")
-    
 def main(args):
     weights_file = args.weightsFile
-    dirImages = args.dirImages
-    batch_size = args.batch_size
+    rootPath = args.rootPath
     height = args.height
     width = args.width
-    filePredictionDir = args.filePredictionDir
     
-    if not exists(filePredictionDir):
-        makedirs(filePredictionDir)
+    model = load_model(weights_file)
     
-    datasetList = createIndex(dirImages)
-    
-    if exists(weights_file):
-        CNN_model = load_model(weights_file)
-        predictionMethod(CNN_model, datasetList, dirImages, batch_size, height, width, filePredictionDir)
-    else:
-        print("El archivo del modelo no existe en el directorio establecido.")
-    
-def test_step(model, X_LL_test, X_LH_test, X_HL_test, X_HH_test, filePredictionDir, dataset, path):
-    if isinstance(model, tf.keras.Model):
-        predictions = model([X_LL_test, X_LH_test, X_HL_test, X_HH_test], training=False)
-        
-    elif isinstance(model, tf.lite.Interpreter):
-        input_details = model.get_input_details()
-        output_details = model.get_output_details()
-        
-        N = X_LL_test.shape[0]
-        results = []
-
-        for i in range(N):
-            sample_LL = np.array([X_LL_test[i]]).astype(np.float32)
-            sample_LH = np.array([X_LH_test[i]]).astype(np.float32)
-            sample_HL = np.array([X_HL_test[i]]).astype(np.float32)
-            sample_HH = np.array([X_HH_test[i]]).astype(np.float32)
-                        
-            model.set_tensor(input_details[0]['index'], sample_LL)
-            model.set_tensor(input_details[1]['index'], sample_LH)
-            model.set_tensor(input_details[2]['index'], sample_HL)
-            model.set_tensor(input_details[3]['index'], sample_HH)
-
-            model.invoke()
-            output_data = model.get_tensor(output_details[0]['index'])
-            results.append(output_data)
-
-        predictions = np.squeeze(np.array(results), axis=1)
-    else:
-        raise TypeError("Unsupported model type")
-    
-    confidences = predictions.numpy()
-        
-    # Guardar imágenes incorrectas
-    positive_incertidumbre = []
-    negative_incertidumbre = []
-    positive_predictions = []
-    negative_predictions = []
-
-    for idx, confidence in enumerate(confidences):
-        image_name = dataset[idx]
-        #image_path = join((path.split('/')[-1]).replace('_clonadas',''), image_name.split("_")[0])
-        
-        if confidence[1] > confidence[0]:  # True Positive
-            if confidence[1] >= 0.9:
-                positive_predictions.append(image_name)
-            else:
-                positive_incertidumbre.append(image_name)
-        else:  # True Negative
-            if confidence[0] >= 0.9:
-                negative_predictions.append(image_name)
-            else:
-                negative_incertidumbre.append(image_name)
-                
-    print(f'Predicciones positivas : {len(positive_predictions)}\nPredicciones negativas : {len(negative_predictions)}\n')
-    print(f'Predicciones positivas con incertidumbre: {len(positive_incertidumbre)}\nPredicciones negativas con incertidumbre: {len(negative_incertidumbre)}\n')
-
-    # Save filenames to text files  
-    writeImageInFile(filePredictionDir, 'Real_predictions.txt', positive_predictions)
-    writeImageInFile(filePredictionDir, 'Ataque_predictions.txt', negative_predictions)
-    writeImageInFile(filePredictionDir, 'Real_incertidumbre.txt', positive_incertidumbre)
-    writeImageInFile(filePredictionDir, 'Ataque_incertidumbre.txt', negative_incertidumbre)
+    evaluateFolders(model, rootPath, height, width)
             
-def writeImageInFile(root, file, listNames):
-    with open(join(root, file), 'a') as f:
-        for name in listNames:
-            f.write(f"{name}\n")
+def evaluateFolders(model, root, height, width):
+    try:
+        for idx, file in enumerate(listdir(root)):
+            img = Image.open(join(root, file))
+            
+            jsonWrite = join(root, f'{file}_jsonPredict')
+            
+            X_LL, X_LH, X_HL, X_HH, Y = getEvaluationBatch(img, height, width)
+            score, ocurrences, prediction = evaluate(model, X_LL, X_LH, X_HL, X_HH, Y)
+            # createJson(jsonWrite, basename(root), basename(dirPath), first, score, ocurrences, prediction)
+            print('---------------------------------------------------------------------')
+            print(f'{file}',end='\t')
+            print(f'Score: {score}\tOcurrences: {ocurrences}\tPrediction: {prediction}\n')
+    except:
+        print(f'Archivo: {file} no se pudo procesar.')  
     
+def createJson(path, basename, dirPath, n, score, ocurrences, prediction):
+    results = {
+        "root": {
+            "rootFolder": basename,
+            "dir": dirPath,
+            "imageNumber":  n
+        },
+        "results": {
+            "score": score,
+            "ocurrences": ocurrences,
+            "prediction": prediction
+        }
+    }
+    print(results['results'])
     
-def predictionMethod(model, listInput, path, batch_size, height, width, filePredictionDir):
-    n = len(listInput)
-    steps = ceil(n/batch_size)
-    print()
-    start_time_full = time.time()
-    for i in range(steps):
-        start_time = time.time()
+    with open(path, 'w') as json_file:
+        json.dump(results, json_file, indent=4) 
         
-        start, end = defineEpochRange(i, batch_size, n)
-        
-        X_LL_test, X_LH_test, X_HL_test, X_HH_test = getEvaluationBatch(listInput, path, start, end, batch_size, height, width)
+def evaluate(model, X_LL_test,X_LH_test,X_HL_test,X_HH_test,y_test):
+    model_out = model([X_LL_test, X_LH_test, X_HL_test, X_HH_test], training=False)
+    TP = 0
+    TN = 0
 
-        data = [X_LL_test, X_LH_test, X_HL_test, X_HH_test]
+    for i in range(len(y_test)):
+        if np.argmax(model_out[i, :]) == y_test[i]:
+            TP += 1
+        else:
+            TN += 1
+            
+    if TN == 0:
+        str_label = 'PASS'
         
-        data = np.array(data)
+    elif TN > 0 and TN < 3:
+        str_label = 'WARNING'
         
-        test_step(model, X_LL_test, X_LH_test, X_HL_test, X_HH_test, filePredictionDir, listInput[start:end], path)
-        if i==0:    print()
-        print(f"Predicting {end - start} images ({i + 1}/{steps})", end='\t')
-        print(f'start: {start}\tend: {end}\tTotal Images:{len(listInput)}')
-        
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        minutes = elapsed_time // 60
-        remaining_seconds = elapsed_time % 60
-        print(f"Batch time: {int(minutes)} minutes {remaining_seconds:.2f} seconds")
-        print("------------------------------------")
+    else:
+        str_label = 'FAIL'
     
-    end_time_full = time.time()
-    elapsed_time = end_time_full - start_time_full
-    hours = elapsed_time // 3600
-    elapsed_time %= 3600
-    minutes, seconds = divmod(elapsed_time, 60)
-    print(f"\nTotal prediction time: {int(hours)} hours, {int(minutes)} minutes, {seconds:.2f} seconds.\n\n")     
+    ocurrences = TN
+    precision = round(ocurrences/3, 3)
 
-def createIndex(path):
-    imgList = list(listdir(path))
+    return precision, ocurrences, str_label
+        
+def getEvaluationBatch(img, height, width):
+    X_LL, X_LH, X_HL, X_HH, X_index, Y, totalBatchSize = createElements(1, height, width, 3)
+
+    w, h = img.size
+    if h > w:
+        img = img.resize((height, width))
+    else:
+        img = img.resize((width, height))
+
+    imgGray1 = img.convert('L')
+    wdChk, htChk = imgGray1.size
+    if htChk > wdChk:
+        imgGray1 = imgGray1.rotate(-90, expand=1)
+
+    imgGray2 = imgGray1.transpose(Image.ROTATE_180)
+
+    imgGray3 = imgGray1.transpose(Image.FLIP_LEFT_RIGHT)
     
-    return imgList
-
-def getEvaluationBatch(listInput, path, start, end, batch_size, height, width):
-    X_LL, X_LH, X_HL, X_HH, X_index, Y, totalBatchSize = createElements(end-start, height, width, 1)
-
-    for sampleIndex, f in enumerate(listInput[start:end]):
-        cA, cH, cV, cD = readAndBuildBatch(f, path, height, width)
+    images = [imgGray1, imgGray2, imgGray3]
+    
+    for sampleIndex, img in enumerate(images):
+        
+        cA, cH, cV, cD = imageTransformation(img, height, width)
         X_LL, X_LH, X_HL, X_HH, X_index = transformImage(cA, cH, cV, cD, X_LL, X_LH, X_HL, X_HH, X_index, sampleIndex, height, width)
         
     X_LL = X_LL.reshape((totalBatchSize, height, width, 1))
@@ -169,7 +111,31 @@ def getEvaluationBatch(listInput, path, start, end, batch_size, height, width):
     X_HL = X_HL.reshape((totalBatchSize, height, width, 1))
     X_HH = X_HH.reshape((totalBatchSize, height, width, 1))
 
-    return X_LL, X_LH, X_HL, X_HH
+    return X_LL, X_LH, X_HL, X_HH, Y
+
+def scaleData(inp, minimum, maximum):
+    minMaxScaler = preprocessing.MinMaxScaler(copy=True, feature_range=(minimum,maximum))
+    inp = inp.reshape(-1, 1)
+    inp = minMaxScaler.fit_transform(inp)
+    
+    return inp
+
+def fwdHaarDWT2D(img):
+    coeffs2 = pywt.dwt2(img, 'bior1.3')
+    LL, (HL, LH, HH) = coeffs2
+    
+    return LL, LH, HL, HH
+
+def createElements(batch_size, height, width, multiply):
+    totalBatchSize = batch_size*multiply
+    X_LL = np.zeros((totalBatchSize, width*height))
+    X_LH = np.zeros((totalBatchSize, width*height))
+    X_HL = np.zeros((totalBatchSize, width*height))
+    X_HH = np.zeros((totalBatchSize, width*height))
+    X_index = np.zeros((totalBatchSize, 1))
+    Y = np.ones((totalBatchSize, 1))
+    
+    return X_LL, X_LH, X_HL, X_HH, X_index, Y, totalBatchSize
 
 def transformImage(imgLL, imgLH, imgHL, imgHH, X_LL, X_LH, X_HL, X_HH, X_index, sampleIndex, height, width):    
     imgLL = np.array(imgLL)
@@ -195,22 +161,7 @@ def transformImage(imgLL, imgLH, imgHL, imgHH, X_LL, X_LH, X_HL, X_HH, X_index, 
     
     return X_LL, X_LH, X_HL, X_HH, X_index
 
-def readAndBuildBatch(f, path, height, width):
-    try:
-        cA, cH, cV, cD = imageTransformation(path, f, height, width)
-    except Exception:
-        print(f'No se pudo leer el archivo {f} en la carpeta {path}, por lo que se eliminará.')
-        remove(join(path, f))
-    return cA, cH, cV, cD
-    
-
-def imageTransformation(imageRoot, f, height, width):
-    try:
-        img = Image.open(join(imageRoot, f))
-    except Exception:
-        print(f'Error: couldn\'t read the file {f}. Make sure only images are present in the folder')
-        return None
-
+def imageTransformation(img, height, width):
     w, h = img.size
     img = img.resize((height, width)) if h > w else img.resize((width, height))
     imgGray = img.convert('L')
@@ -244,21 +195,30 @@ def getTiffFromJpg(img):
     tiff_bytes_io.seek(0)
     
     return tiff_bytes_io
+    
+def load_model(model_path):
+    model_extension = Path(model_path).suffix.lower()
+
+    if model_extension in ['.h5', '.keras']:
+        model = tf.keras.models.load_model(model_path, compile=False)
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        print(f"Modelo {model_extension} encontrado y cargado correctamente!\n")
+        return model
+    
+    else:
+        raise ValueError(f"Unsupported model file format: {model_extension}")
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     
     parser.add_argument('--weightsFile', type=str, help='Saved CNN model file', default='./checkpoint/cp.h5')
     
-    parser.add_argument('--dirImages', type=str, help='Directory with images.')
+    parser.add_argument('--rootPath', type=str, help='Directory with (Moiré pattern) images.', default='./')
     
-    parser.add_argument('--batch_size', type=int, help='Batch size for epoch in training', default=32)
-    parser.add_argument('--height', type=int, help='Image height resize', default=375)
-    parser.add_argument('--width', type=int, help='Image width resize', default=500)
-    
-    parser.add_argument('--filePredictionDir', type=str, help='Directory to save predictions.', default='./predicciones')
-    
+    parser.add_argument('--height', type=int, help='Image height resize', default=800)
+    parser.add_argument('--width', type=int, help='Image width resize', default=1400)
+        
     return parser.parse_args(argv)
-
+    
 if __name__ == '__main__':
     main(parse_arguments(sys.argv[1:]))
--- src/mCNN.py
+++ src/mCNN.py
@@ -2,11 +2,11 @@
 import os
 
 from keras.models import Model # type: ignore
-from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Activation, Flatten, Add, Multiply, Maximum # type: ignore
+from keras.layers import Input, Convolution2D, AveragePooling2D, Dense, Dropout, Concatenate, Flatten, Multiply, Maximum # type: ignore
 
 from keras import regularizers
 
-def createModel(height, width, depth, num_classes):
+def createHRNetModel(height, width, depth, num_classes):
     kernel_size_1 = 7
     kernel_size_2 = 3
     pool_size = 2
@@ -21,25 +21,35 @@ def createModel(height, width, depth, num_classes):
     inpHL = Input(shape=(height, width, depth))
     inpHH = Input(shape=(height, width, depth))
 
+    # Initial Convolutions
     conv_1_LL = Convolution2D(conv_depth_1, (kernel_size_1, kernel_size_1), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(inpLL)
     conv_1_LH = Convolution2D(conv_depth_1, (kernel_size_1, kernel_size_1), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(inpLH)
     conv_1_HL = Convolution2D(conv_depth_1, (kernel_size_1, kernel_size_1), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(inpHL)
     conv_1_HH = Convolution2D(conv_depth_1, (kernel_size_1, kernel_size_1), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(inpHH)
-    pool_1_LL = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1_LL)
-    pool_1_LH = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1_LH)
-    pool_1_HL = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1_HL)
-    pool_1_HH = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1_HH)
+
+    # Max Pooling
+    pool_1_LL = AveragePooling2D(pool_size=(pool_size, pool_size))(conv_1_LL)
+    pool_1_LH = AveragePooling2D(pool_size=(pool_size, pool_size))(conv_1_LH)
+    pool_1_HL = AveragePooling2D(pool_size=(pool_size, pool_size))(conv_1_HL)
+    pool_1_HH = AveragePooling2D(pool_size=(pool_size, pool_size))(conv_1_HH)
 
     avg_LH_HL_HH = Maximum()([pool_1_LH, pool_1_HL, pool_1_HH])
     inp_merged = Multiply()([pool_1_LL, avg_LH_HL_HH])
-    C4 = Convolution2D(conv_depth_2, (kernel_size_2, kernel_size_2), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(inp_merged)
-    S2 = MaxPooling2D(pool_size=(4, 4))(C4)
+
+    # Multi-scale Feature Fusion
+    scale_1 = Convolution2D(conv_depth_2, (kernel_size_2, kernel_size_2), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(inp_merged)
+    scale_2 = Convolution2D(conv_depth_2, (kernel_size_2 + 2, kernel_size_2 + 2), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(inp_merged)
+    scale_3 = Convolution2D(conv_depth_2, (kernel_size_2 + 4, kernel_size_2 + 4), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(inp_merged)
+
+    fused = Concatenate()([scale_1, scale_2, scale_3])
+    S2 = AveragePooling2D(pool_size=(4, 4))(fused)
+
     drop_1 = Dropout(drop_prob_1)(S2)
     C5 = Convolution2D(conv_depth_1, (kernel_size_2, kernel_size_2), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(drop_1)
-    S3 = MaxPooling2D(pool_size=(pool_size, pool_size))(C5)
+    S3 = AveragePooling2D(pool_size=(pool_size, pool_size))(C5)
     C6 = Convolution2D(conv_depth_1, (kernel_size_2, kernel_size_2), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))(S3)
-    S4 = MaxPooling2D(pool_size=(pool_size, pool_size))(C6)
-    drop_2 = Dropout(drop_prob_1)(S4)
+    S4 = AveragePooling2D(pool_size=(pool_size, pool_size))(C6)
+    drop_2 = Dropout(drop_prob_2)(S4)
     flat = Flatten()(drop_2)
     hidden = Dense(hidden_size, activation='relu', kernel_regularizer=regularizers.l2(0.01))(flat)
     drop_3 = Dropout(drop_prob_2)(hidden)
