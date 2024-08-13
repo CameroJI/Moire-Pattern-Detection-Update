c5ae84d7625c1d952a3d99cbf135c0a8ccee6ea4
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-08-13 10:51:36 -0600

    changes

--- src/haar2D.py
+++ src/haar2D.py
@@ -8,14 +8,5 @@ from skimage.filters import gabor
 def fwdHaarDWT2D(img):
     coeffs2 = pywt.dwt2(img, 'bior1.3')
     LL, (HL, LH, HH) = coeffs2
-    gaborImg = gaborFilter(img)
     
-    return gaborImg, LH, HL, HH
-
-def gaborFilter(img, frequency=0.56, theta=pi/2):
-    filt_real, filt_imag = gabor(np.array(img, dtype=np.float32), frequency=frequency, theta=theta)
-    
-    magnitude = np.sqrt(filt_real**2 + filt_imag**2)
-    magnitude = (magnitude - np.min(magnitude)) / (np.max(magnitude) - np.min(magnitude)) * 255
-    
-    return magnitude
+    return LL, LH, HL, HH
