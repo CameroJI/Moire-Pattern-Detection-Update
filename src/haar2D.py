1a4cf2ee25c34fa9063baa301ad403560771d255
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-08-26 09:11:52 -0600

    Update haar2D.py

--- src/haar2D.py
+++ src/haar2D.py
@@ -3,7 +3,6 @@ import pywt.data
 
 # computes the homography coefficients for PIL.Image.transform using point correspondences
 def fwdHaarDWT2D(img):
-    coeffs2 = pywt.dwt2(img, 'bior1.3')
-    LL, (HL, LH, HH) = coeffs2
-    
+    coeffs2 = pywt.wavedec2(img, 'bior2.2', level=3)
+    LL, (HL, LH, HH) = coeffs2[0], coeffs2[1]
     return LL, LH, HL, HH
