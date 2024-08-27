ab920b2be41db899ab9b1724f35d2a2520309835
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-08-27 09:06:52 -0600

    Update createTrainingData.py

--- src/createTrainingData.py
+++ src/createTrainingData.py
@@ -32,16 +32,15 @@ def transformImageAndSave(image, f, customStr, path):
     fLH = f.replace(fileName, f'{fileName}_{customStr}LH').replace(os.path.splitext(f)[-1], '.tiff')
     fHL = f.replace(fileName, f'{fileName}_{customStr}HL').replace(os.path.splitext(f)[-1], '.tiff')
     fHH = f.replace(fileName, f'{fileName}_{customStr}HH').replace(os.path.splitext(f)[-1], '.tiff')
-    
     cA = Image.fromarray(cA)
     cH = Image.fromarray(cH)
     cV = Image.fromarray(cV)
     cD = Image.fromarray(cD)
     
-    cA = cA.resize(width, height)
-    cH = cH.resize(width, height)
-    cV = cV.resize(width, height)
-    cD = cD.resize(width, height)
+    cA = cA.resize((width, height), Image.LANCZOS)
+    cH = cH.resize((width, height), Image.LANCZOS)
+    cV = cA.resize((width, height), Image.LANCZOS)
+    cD = cA.resize((width, height), Image.LANCZOS)
     
     cA.save(join(path, fLL))
     cH.save(join(path, fLH))
@@ -74,9 +73,6 @@ def augmentAndTransformImage(f, mainFolder, trainFolder):
 def PreprocessImage(imgPath, width, height):
     img = Image.open(imgPath)
     w, h = img.size
-    
-    width *= 8
-    height *= 8
     
     if w < width or h < height:
         proportion = min(width / w, height / h)
