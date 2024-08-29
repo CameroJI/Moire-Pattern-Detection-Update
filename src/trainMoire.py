eb7d1316e27b9bb2c63c49acf2809725bf3ce26d
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-08-29 11:23:08 -0600

    Update trainMoire.py

--- src/trainMoire.py
+++ src/trainMoire.py
@@ -71,7 +71,7 @@ def main(args):
     class_weights = {0: 2.0, 1: 1.0}
     
     model.fit(X_train, epochs=numEpochs,
-              callbacks=[epochCheckpointCallback, batchCheckpointCallback, timingCallback], class_weights=class_weights)
+              callbacks=[epochCheckpointCallback, batchCheckpointCallback, timingCallback], class_weight=class_weights)
 
 def getModel(loadFlag, path):
     if not exists(path):
