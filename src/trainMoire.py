3dd1e71d8f9b2cc58134a140e3b497988abf450d
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-08-30 12:28:17 -0600

    Update trainMoire.py

--- src/trainMoire.py
+++ src/trainMoire.py
@@ -146,10 +146,10 @@ def preprocessImage(image):
 
     LL, LH, HL, HH = tf_wavelet_transform(image)
     
-    LL_resized = resize(LL, HEIGHT, WIDTH)
-    LH_resized = resize(LH, HEIGHT, WIDTH)
-    HL_resized = resize(HL, HEIGHT, WIDTH)
-    HH_resized = resize(HH, HEIGHT, WIDTH)
+    LL_resized = resize(LL, HEIGHT/8, WIDTH)
+    LH_resized = resize(LH, HEIGHT/8, WIDTH)
+    HL_resized = resize(HL, HEIGHT/8, WIDTH)
+    HH_resized = resize(HH, HEIGHT/8, WIDTH)
     
     wavelet_tensor = tf.stack([LL_resized, LH_resized, HL_resized, HH_resized], axis=-1)
     
@@ -165,7 +165,7 @@ def getModel(loadFlag, path):
         #     if isinstance(layer, Dense):
         #         layer.trainable = True
     else:
-        model = createMobileModel(height=HEIGHT, width=WIDTH, depth=1)
+        model = createMobileModel(height=HEIGHT/8, width=WIDTH/8, depth=1)
         
     return model
 
