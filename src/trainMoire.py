1b60795b086216ecedadef1ba7a7d2ba343e4f83
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-09-03 18:15:34 -0600

    Update trainMoire.py

--- src/trainMoire.py
+++ src/trainMoire.py
@@ -143,14 +143,6 @@ def getModel(loadFlag, path):
         
     return model
 
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1', 'True'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0', 'False'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     
